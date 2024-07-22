33f0784c39fc328a4b1a599efacd3fa863c8cb14
SubhabrataDas <das.subhabrata85@gmail.com>
2024-07-22 10:40:33 +0200

    Updated text on Ledger UI

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YF3WJZEM.mjs";import"./chunk-RSKTCQGK.mjs";import"./chunk-YMSJO7EN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QS5TJJCT.mjs";import"./chunk-RSKTCQGK.mjs";import"./chunk-YMSJO7EN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
